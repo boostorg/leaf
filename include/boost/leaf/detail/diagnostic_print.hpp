2fe599fdf352c964c9d14b0088754a7cce7fe80e
Emil Dotchevski <emildotchevski@gmail.com>
2018-09-16 19:26:41 -0700

    copyright messages

--- include/boost/leaf/detail/diagnostic_print.hpp
+++ include/boost/leaf/detail/diagnostic_print.hpp
@@ -1,4 +1,4 @@
-//Copyright (c) 2006-2009 Emil Dotchevski and Reverge Studios, Inc.
+//Copyright (c) 2018 Emil Dotchevski
 //Copyright (c) 2018 Second Spectrum, Inc.
 
 //Distributed under the Boost Software License, Version 1.0. (See accompanying
