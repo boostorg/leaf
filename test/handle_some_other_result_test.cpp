06a236f39ced1784e4b68154d801b422a7b3016d
Emil Dotchevski <emildotchevski@gmail.com>
2019-01-25 09:46:22 -0800

    travis

--- test/handle_some_other_result_test.cpp
+++ test/handle_some_other_result_test.cpp
@@ -5,66 +5,22 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/leaf/handle.hpp>
+#include "_test_res.hpp"
 #include "boost/core/lightweight_test.hpp"
 
 namespace leaf = boost::leaf;
 
 template <int> struct info { int value; };
 
-template <class T, class E>
-class result
-{
-	enum class variant
-	{
-		value,
-		error
-	};
-	T value_;
-	E error_;
-	variant which_;
-public:
-	result( T const & value ) noexcept:
-		value_(value),
-		which_(variant::value)
-	{
-	}
-	result( E const & error ) noexcept:
-		error_(error),
-		which_(variant::error)
-	{
-	}
-	explicit operator bool() const noexcept
-	{
-		return which_==variant::value;
-	}
-	T const & value() const
-	{
-		assert(which_==variant::value);
-		return value_;
-	}
-	E const & error() const
-	{
-		assert(which_==variant::error);
-		return error_;
-	}
-};
-
-namespace boost { namespace leaf {
-	template <class T, class E>
-	struct is_result_type<result<T, E>>: std::true_type
-	{
-	};
-} }
-
-result<int,std::error_code> f( bool succeed )
+res<int,std::error_code> f( bool succeed )
 {
 	if( succeed )
 		return 42;
 	else
-		return make_error_code(std::errc::no_such_file_or_directory);
+		return make_error_code(errc_a::a0);
 }
 
-result<int,std::error_code> g( bool succeed )
+res<int,std::error_code> g( bool succeed )
 {
 	if( auto r = f(succeed) )
 		return r;
@@ -75,7 +31,7 @@ result<int,std::error_code> g( bool succeed )
 int main()
 {
 	{
-		result<int,std::error_code> r = leaf::handle_some(
+		res<int,std::error_code> r = leaf::handle_some(
 			[ ]
 			{
 				return g(true);
@@ -84,7 +40,8 @@ int main()
 		BOOST_TEST(r.value()==42);
 	}
 	{
-		result<int,std::error_code> r = leaf::handle_some(
+		int called = 0;
+		res<int,std::error_code> r = leaf::handle_some(
 			[&]
 			{
 				auto r = g(false);
@@ -94,13 +51,15 @@ int main()
 				BOOST_TEST(!std::strcmp(ec.category().name(),"LEAF error, use with leaf::handle_some or leaf::handle_all."));
 				return r;
 			},
-			[ ]( info<42> const & x, leaf::match<leaf::condition<std::errc>, std::errc::no_such_file_or_directory> ec )
+			[&]( info<42> const & x, leaf::match<leaf::condition<cond_x>, cond_x::x00> ec )
 			{
+				called = 1;
 				BOOST_TEST(x.value==42);
 				return ec.value();
 			} );
 		BOOST_TEST(!r);
-		BOOST_TEST(r.error() == make_error_code(std::errc::no_such_file_or_directory));
+		BOOST_TEST(r.error() == make_error_code(errc_a::a0));
+		BOOST_TEST(called);
 	}
 	return boost::report_errors();
 }
