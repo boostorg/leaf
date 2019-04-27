bc30c4258a0ce96e273feb212bff76040e38b303
Emil Dotchevski <emildotchevski@gmail.com>
2019-04-27 12:06:38 -0700

    Removing g++-4.9 workarounds

--- include/boost/leaf/detail/mp11.hpp
+++ include/boost/leaf/detail/mp11.hpp
@@ -9,9 +9,13 @@
 //  See accompanying file LICENSE_1_0.txt or copy at
 //  http://www.boost.org/LICENSE_1_0.txt
 
+// LEAF needs a few mp11 utilities, which have been copied into the internal
+// namespace boost::leaf::leaf_detail_mp11 in order to avoid a dependency
+// on the entire mp11 library. The BOOST_LEAF_USE_MP11 configuration macro
+// tells LEAF to not bother and just use <boost/mp11/algorithm.hpp> instead.
 #ifdef BOOST_LEAF_USE_BOOST_MP11
 
-#include <boost/mp11.hpp>
+#include <boost/mp11/algorithm.hpp>
 
 namespace boost { namespace leaf { namespace leaf_detail_mp11 = ::boost::mp11; } }
 
