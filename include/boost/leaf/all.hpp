c67a2dfe330133822b74a327fe052bb89d184193
unknown <emildotchevski@gmail.com>
2020-07-08 10:16:52 -0700

    Using std::is_constructible for SFINAE

--- include/boost/leaf/all.hpp
+++ include/boost/leaf/all.hpp
@@ -4702,8 +4702,6 @@ namespace boost { namespace leaf {
 			return what_.kind()==result_discriminant::ctx_ptr ? ctx_->captured_id_ : what_.get_error_id();
 		}
 
-		static int init_T_with_U( T && );
-
 	public:
 
 		BOOST_LEAF_CONSTEXPR result( result && x ) noexcept:
@@ -4743,7 +4741,7 @@ namespace boost { namespace leaf {
 
 		// SFINAE: T can be initialized with a U, e.g. result<std::string>("literal")
 		template <class U>
-		BOOST_LEAF_CONSTEXPR result( U && u, decltype(init_T_with_U(std::forward<U>(u))) * = 0 ):
+		BOOST_LEAF_CONSTEXPR result( U && u, typename std::enable_if<std::is_constructible<T, U>::value, int>::type * = 0 ):
 			stored_(std::forward<U>(u)),
 			what_(result_discriminant::kind_val{})
 		{
