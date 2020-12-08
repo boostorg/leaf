b90ddd52530f4d0578815497384cccb1413862ed
Emil Dotchevski <emildotchevski@gmail.com>
2020-12-08 13:35:52 -0800

    Issue #17

--- include/boost/leaf/error.hpp
+++ include/boost/leaf/error.hpp
@@ -43,12 +43,12 @@
     BOOST_LEAF_ASSIGN(auto v, r)
 
 #define BOOST_LEAF_CHECK(r)\
-    {\
-        static_assert(::boost::leaf::is_result_type<typename std::decay<decltype(r)>::type>::value, "BOOST_LEAF_CHECK requires a result type");\
-        auto && BOOST_LEAF_TMP = r;\
-        if( !BOOST_LEAF_TMP )\
-            return BOOST_LEAF_TMP.error();\
-    }
+    static_assert(::boost::leaf::is_result_type<typename std::decay<decltype(r)>::type>::value, "BOOST_LEAF_CHECK requires a result type");\
+    auto && BOOST_LEAF_TMP = r;\
+    if( BOOST_LEAF_TMP )\
+        ;\
+    else\
+        return BOOST_LEAF_TMP.error()
 
 #define BOOST_LEAF_NEW_ERROR ::leaf::leaf_detail::inject_loc{__FILE__,__LINE__,__FUNCTION__}+::boost::leaf::new_error
 
