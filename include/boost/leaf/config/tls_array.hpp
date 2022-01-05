d9a09a38d53ca5940139313e1ba49977ab0bc821
Emil Dotchevski <emildotchevski@gmail.com>
2022-01-04 20:00:53 -0800

    Added copyright message

--- include/boost/leaf/config/tls_array.hpp
+++ include/boost/leaf/config/tls_array.hpp
@@ -2,6 +2,7 @@
 #define BOOST_LEAF_CONFIG_TLS_ARRAY_HPP_INCLUDED
 
 /// Copyright (c) 2018-2021 Emil Dotchevski and Reverge Studios, Inc.
+/// Copyright (c) 2022 Khalil Estell
 
 /// Distributed under the Boost Software License, Version 1.0. (See accompanying
 /// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
