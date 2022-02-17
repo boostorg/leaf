20bcc54ee4c48da8c0469242c438065dba970f61
zajo <emildotchevski@gmail.com>
2022-02-17 20:24:19 +0000

    Deploying to gh-pages from  @ b2f95dd7b35baaa170ae05841e4d450d87b74a13 🚀

--- leaf.hpp
+++ leaf.hpp
@@ -3,7 +3,7 @@
 
 // LEAF single header distribution. Do not edit.
 
-// Generated on 02/01/2022 from https://github.com/boostorg/leaf/tree/e9eb512.
+// Generated on 02/17/2022 from https://github.com/boostorg/leaf/tree/b2f95dd.
 // Latest version of this file: https://raw.githubusercontent.com/boostorg/leaf/gh-pages/leaf.hpp.
 
 // Copyright 2018-2022 Emil Dotchevski and Reverge Studios, Inc.
@@ -5539,6 +5539,58 @@ struct is_result_type<result<T>>: std::true_type
 };
 
 } }
+
+#endif
+// >>> #include <boost/leaf/to_variant.hpp>
+#line 1 "boost/leaf/to_variant.hpp"
+#ifndef BOOST_LEAF_TO_VARIANT_HPP_INCLUDED
+#define BOOST_LEAF_TO_VARIANT_HPP_INCLUDED
+
+// Copyright 2018-2022 Emil Dotchevski and Reverge Studios, Inc.
+
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#if __cplusplus >= 201703L
+
+// Expanded at line 16: #include <boost/leaf/config.hpp>
+// Expanded at line 3663: #include <boost/leaf/handle_errors.hpp>
+// Expanded at line 4918: #include <boost/leaf/result.hpp>
+#include <variant>
+#include <optional>
+#include <tuple>
+
+namespace boost { namespace leaf {
+
+template <class... E, class TryBlock>
+std::variant<typename std::decay<decltype(std::declval<TryBlock>()().value())>::type,std::tuple<std::optional<E>...>>
+to_variant( TryBlock && try_block )
+{
+    static_assert(is_result_type<decltype(std::declval<TryBlock>()())>::value, "The return type of the try_block passed to a to_variant function must be registered with leaf::is_result_type");
+    using T = typename std::decay<decltype(std::declval<TryBlock>()().value())>::type;
+    using error_tuple_type = std::tuple<std::optional<E>...>;
+    using variant_type = std::variant<T, error_tuple_type>;
+    return try_handle_all(
+        [&]() -> result<variant_type>
+        {
+            if( auto r = std::forward<TryBlock>(try_block)() )
+                return *std::move(r);
+            else
+                return r.error();
+        },
+        []( E const * ... e ) -> variant_type
+        {
+            return error_tuple_type { e ? std::optional<E>(*e) : std::optional<E>{}... };
+        },
+        []() -> variant_type
+        {
+            return error_tuple_type { };
+        } );
+}
+
+} }
+
+#endif
 
 #endif
 
