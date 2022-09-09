9a6f481561e26881f07f482efe29b9f553261380
zajo <emildotchevski@gmail.com>
2022-09-09 04:02:40 +0000

    Deploying to gh-pages from  @ 9d3ffa0f1c859a34da6175b53572fd4f2a61bb18 🚀

--- leaf.hpp
+++ leaf.hpp
@@ -3,7 +3,7 @@
 
 // LEAF single header distribution. Do not edit.
 
-// Generated on 09/07/2022 from https://github.com/boostorg/leaf/tree/b2090ac.
+// Generated on 09/09/2022 from https://github.com/boostorg/leaf/tree/9d3ffa0.
 // Latest version of this file: https://raw.githubusercontent.com/boostorg/leaf/gh-pages/leaf.hpp.
 
 // Copyright 2018-2022 Emil Dotchevski and Reverge Studios, Inc.
