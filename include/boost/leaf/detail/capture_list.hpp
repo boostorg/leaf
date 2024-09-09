d0047b721c0aa6e7512ece2da4e20117fa224455
Emil Dotchevski <emildotchevski@gmail.com>
2024-09-08 18:26:05 -0700

    Updated copyright dates

--- include/boost/leaf/detail/capture_list.hpp
+++ include/boost/leaf/detail/capture_list.hpp
@@ -1,7 +1,7 @@
 #ifndef BOOST_LEAF_DETAIL_CAPTURE_HPP_INCLUDED
 #define BOOST_LEAF_DETAIL_CAPTURE_HPP_INCLUDED
 
-// Copyright 2018-2023 Emil Dotchevski and Reverge Studios, Inc.
+// Copyright 2018-2024 Emil Dotchevski and Reverge Studios, Inc.
 
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
