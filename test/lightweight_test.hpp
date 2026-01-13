91a72958d99ad90fa37ed3e57bfbc8060613d93b
Emil Dotchevski <emildotchevski@gmail.com>
2026-01-13 01:02:45 -0500

    removing [[noreturn]] attribute from throw_exception definition

--- test/lightweight_test.hpp
+++ test/lightweight_test.hpp
@@ -8,14 +8,14 @@
 #ifdef BOOST_LEAF_NO_EXCEPTIONS
 namespace boost
 {
-    [[noreturn]] inline void throw_exception( std::exception const & e )
+    inline void throw_exception( std::exception const & e )
     {
         std::cerr << "Terminating due to a C++ exception under BOOST_LEAF_NO_EXCEPTIONS: " << e.what();
         std::terminate();
     }
 
     struct source_location;
-    [[noreturn]] inline void throw_exception( std::exception const & e, boost::source_location const & )
+    inline void throw_exception( std::exception const & e, boost::source_location const & )
     {
         throw_exception(e);
     }
